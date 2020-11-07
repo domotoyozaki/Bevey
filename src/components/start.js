3c55ca368e469d852c37d83c2591a402f33eb79d
Mark Tran <markmtran1@gmail.com>
2020-11-07 15:59:05 -0500

    get started page in progress. Need to fix font & button

--- src/components/start.js
+++ src/components/start.js
@@ -1,12 +1,25 @@
-import { Grid, Typography } from '@material-ui/core';
+import './start.css';
+import { Grid, Typography, Button } from '@material-ui/core';
 import React from 'react';
 import logo from 'images/mainLogo.png';
 
 const Start = () => {
   return (
-    <Grid>
-      <Typography variant='h1'>Welcome to Bevey!</Typography>
-      <img src={logo} alt='Bevey Main Logo'/>
+    <Grid container alignItems='center' justify='center' direction='column' style={{height: '100vh'}}>
+      <Grid container direction='column' alignItems='center' spacing={9}>
+
+        <Grid item>
+          <Typography className='comfortaa white-text' variant='h2' align='center'>Welcome to Bevey!</Typography>
+        </Grid>
+
+        <Grid item justify='center' alignItems='center'>
+          <img style={{ width: '250px', height: 'auto', margins: '0 auto 0 auto' }} src={logo} alt='Bevey Main Logo'/>
+        </Grid>
+
+        <Grid item>
+          <Button className='comfortaa white-text' size='large'>Get Started</Button>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
